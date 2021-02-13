4d315223b91f376dda9c6ce600366d5f8cadf2ed
Neupane07 <hbkbimal07@gmail.com>
2021-02-13 23:45:59 +0530

    added approval cards with accept and reject buttons

--- src/index.js
+++ src/index.js
@@ -2,28 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Bimal"
-        timeAgo="Today at 5:30PM"
-        img={faker.image.image()}
-        content={"Nice!"}
-      />
-      <CommentDetail
-        author="Akhil"
-        timeAgo="Today at 1:00AM"
-        img={faker.image.image()}
-        content={"Well done man!"}
-      />
-      <CommentDetail
-        author="Hel"
-        timeAgo="Yesterday at 4:33PM"
-        img={faker.image.image()}
-        content={"Chapri!"}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Bimal"
+          timeAgo="Today at 5:30PM"
+          img={faker.image.image()}
+          content={"Nice!"}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Akhil"
+          timeAgo="Today at 1:00AM"
+          img={faker.image.image()}
+          content={"Well done man!"}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Hel"
+          timeAgo="Yesterday at 4:33PM"
+          img={faker.image.image()}
+          content={"Chapri!"}
+        />
+      </ApprovalCard>
     </div>
   );
 };
